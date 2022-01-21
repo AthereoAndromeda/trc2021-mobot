355b3e88a8aacdf2924dc7357f4979bcfe006bba
Angelo <athereoandromeda@gmail.com>
2022-01-21 18:32:44 +0800

    Refactor and cleanup comments

--- _N_taskMovements.ino
+++ _N_taskMovements.ino
@@ -1,77 +1,26 @@
 void movement1() {
-  //  if (test_directions)
-  //  {
-  //    digitalWrite(LED_BUILTIN, HIGH);
   run(Forward, RUN_DELAY);
-  run(Stop, 0);
-
-  //    stopMotors();
-
-  //    test_directions = false;
-  //  }
-  //  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void movement2() {
-  //  if (test_directions)
-  //  {
-  //    digitalWrite(LED_BUILTIN, HIGH);
   run(Backward, RUN_DELAY);
-  //    run(Stop, 0);
-
-  //    stopMotors();
-
-  //    test_directions = false;
-  //  }
-  //  digitalWrite(LED_BUILTIN, LOW);
+  run(Backward, RUN_DELAY);
 }
 
 void movement3() {
-  //  if (test_directions)
-  //  {
-  //    digitalWrite(LED_BUILTIN, HIGH);
   run(CW_Center_Center, RUN_DELAY);
-  //    run(Stop, 0);
-
-  //    stopMotors();
-
-  //    test_directions = false;
-  //  }
-  //  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void movement4() {
-  //    digitalWrite(LED_BUILTIN, HIGH);
   run(CCW_Center_Center, RUN_DELAY);
-  //    run(Stop, 0);
-  //    stopMotors();
-
-  //  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void movement5() {
   run(Left, RUN_DELAY);
   run(Left, RUN_DELAY);
-  //    run(Stop, 0);
-
-  //    stopMotors();
-
-  //    test_directions = false;
-  //  }
-  //  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void movement6() {
-  //  if (test_directions)
-  //  {
-  //    digitalWrite(LED_BUILTIN, HIGH);
   run(Right, RUN_DELAY);
   run(Right, RUN_DELAY);
-  //    run(Stop, 0);
-
-  //    stopMotors();
-
-  //    test_directions = false;
-  //  }
-  //  digitalWrite(LED_BUILTIN, LOW);
 }
